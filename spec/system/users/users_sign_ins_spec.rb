61e6e3207b586575c28c9f171069ab08d0e798ba
igorparente <igorscheidegger@hotmail.com>
2022-02-11 14:09:59 -0300

    system test working

--- spec/system/users/users_sign_ins_spec.rb
+++ spec/system/users/users_sign_ins_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe "Users::SignIns", type: :system do
   end
 
   it "User pode fazer login" do
-    visit "/bank_accounts"
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :first_name => 'Exemplo Name')
 
-    fill_in "Email", :with => "exemplo@exemplo.com"
-    fill_in "Password", :with => "123456"
+    visit "/users/sign_in"
+    fill_in "Email", :with => user.email
+    fill_in "Password", :with => user.password
     click_button "Log in"
 
-    # expect(page).to have_text('My Accounts')
-
+    expect(page).to have_text('Signed in successfully.')
   end
 
   it "User pode fazer criar login" do
@@ -21,18 +21,47 @@ RSpec.describe "Users::SignIns", type: :system do
 
 
     click_link "Forgot your password?"
-
   end
 
   it "User pode lembrar login" do
     visit "/users/sign_in"
 
     check "Remember me"
-
   end
 
-  it "User pode visitar a pagna bank_accounts" do
+  it "User pode visitar a pagia bank_accounts" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :first_name => 'Exemplo Name')
+    login_as(user, :scope => :user)
+    visit "/bank_accounts"
 
+    expect(page).to have_text('My accounts')
+  end
+
+  it "User pode visitar a pagia bank_accounts e criar conta" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :first_name => 'Exemplo Name')
+    login_as(user, :scope => :user)
+
+    visit "/bank_accounts"
+    click_link "Create Account"
+
+    expect(page).to have_button 'Create New Account'
+  end
+
+  it "User pode visitar bank_accounts/new criar conta e ver conta" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :first_name => 'Exemplo Name')
+    login_as(user, :scope => :user)
+
+    visit "/bank_accounts/new"
+    click_button "Create New Account"
+
+    expect(page).to have_text('Balance')
+    expect(page).to have_link 'Filter Transaction'
+    expect(page).to have_link 'Make Transaction'
+    expect(page).to have_link 'My Accounts'
+    expect(page).to have_text('Transaction Date')
+    expect(page).to have_text('Transaction Type')
+    expect(page).to have_text('Transaction Amount')
+    expect(page).to have_text('Balance of the Day')
   end
 
 end
