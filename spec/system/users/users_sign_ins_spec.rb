e22ead65819e110fc97ac48a5f3cbb333af8a78a
igorparente <igorscheidegger@hotmail.com>
2022-02-11 15:58:59 -0300

    system test done

--- spec/system/users/users_sign_ins_spec.rb
+++ spec/system/users/users_sign_ins_spec.rb
@@ -64,4 +64,64 @@ RSpec.describe "Users::SignIns", type: :system do
     expect(page).to have_text('Balance of the Day')
   end
 
+  it "User pode visitar trasactions/new e fazer depósito" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :first_name => 'Exemplo Name')
+    login_as(user, :scope => :user)
+
+    bank_account = BankAccount.create!(user_id: user.id)
+
+    visit "/bank_accounts/#{bank_account.id}/transactions/new"
+
+    select "Deposit", :from => "Transaction type"
+    fill_in "Amount", :with => '100'
+    click_button 'Create Transaction'
+
+    expect(page).to have_text('The amount has been credited.')
+  end
+
+  it "User pode visitar trasactions/new e fazer saque" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :first_name => 'Exemplo Name')
+    login_as(user, :scope => :user)
+
+    bank_account = BankAccount.create!(user_id: user.id, :balance => 1000)
+
+    visit "/bank_accounts/#{bank_account.id}/transactions/new"
+
+    select "Withdraw", :from => "Transaction type"
+    fill_in "Amount", :with => '100'
+    click_button 'Create Transaction'
+
+    expect(page).to have_text('The amount has been debited.')
+  end
+
+  it "User pode visitar trasactions/new e fazer transferência" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :first_name => 'Exemplo Name')
+    bank_account = BankAccount.create!(user_id: user.id, :balance => 1000)
+    login_as(user, :scope => :user)
+    
+    user_2 = User.create!(:email => 'test2@example.com', :password => 'f4k3p455w0rd', :first_name => 'Exemplo Name')
+    bank_account_2 = BankAccount.create!(user_id: user_2.id, :account_number => '272200')
+
+    visit "/bank_accounts/#{bank_account.id}/transactions/new"
+
+    select "Transfer", :from => "Transaction type"
+    fill_in "Account Number", visible: false, :with => bank_account_2.account_number
+    fill_in "Amount", :with => '100'
+    click_button 'Create Transaction'
+
+    expect(page).to have_text('Transfer successful.')
+  end
+
+  it "User pode visitar /transactions e filtrar transações" do
+    user = User.create!(:email => 'test@example.com', :password => 'f4k3p455w0rd', :first_name => 'Exemplo Name')
+    login_as(user, :scope => :user)
+
+    bank_account = BankAccount.create!(user_id: user.id)
+
+    visit "/bank_accounts/#{bank_account.id}/transactions"
+
+    fill_in "query", :with => '01/01/2022'
+    fill_in "query_2", :with => '01/02/2022'
+    click_button 'Search'
+  end
 end
