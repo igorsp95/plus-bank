b86149fad9427a729e92070b4940a4ce849fa577
igorparente <igorscheidegger@hotmail.com>
2022-02-11 13:08:26 -0300

    making tests

--- spec/system/users/users_sign_ins_spec.rb
+++ spec/system/users/users_sign_ins_spec.rb
@@ -6,19 +6,20 @@ RSpec.describe "Users::SignIns", type: :system do
   end
 
   it "User pode fazer login" do
-    visit "/users/sign_in"
+    visit "/bank_accounts"
 
     fill_in "Email", :with => "exemplo@exemplo.com"
     fill_in "Password", :with => "123456"
     click_button "Log in"
+
+    # expect(page).to have_text('My Accounts')
 
   end
 
   it "User pode fazer criar login" do
     visit "/users/sign_in"
 
-    fill_in "Email", :with => "exemplo@exemplo.com"
-    fill_in "Password", :with => "123456"
+
     click_link "Forgot your password?"
 
   end
@@ -26,16 +27,11 @@ RSpec.describe "Users::SignIns", type: :system do
   it "User pode lembrar login" do
     visit "/users/sign_in"
 
-    fill_in "Email", :with => "exemplo@exemplo.com"
-    fill_in "Password", :with => "123456"
     check "Remember me"
 
   end
 
-  it "User pode lembrar login" do
-    visit "/bank_accounts"
-
-    click_button "Create Account"
+  it "User pode visitar a pagna bank_accounts" do
 
   end
 
